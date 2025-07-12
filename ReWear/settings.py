660167fcf67785ecf783c817257a1c055e81af07
Polymath-Saksh <sakshamkumarprasad03@gmail.com>
2025-07-12 10:14:25 +0530

    Added subapps

--- ReWear/settings.py
+++ ReWear/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core","adminpanel","items","swaps","users","dashboard"
 ]
 
 MIDDLEWARE = [
